86ef7c2c152a98715a4e9f2119c7cbd6a5f85dc0
mrawrcat <wdan13@gmail.com>
2020-09-10 16:26:15 -0400

    Added fading transition

---
+++ Assets/Scripts/FadeUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeUI : MonoBehaviour
+{
+    //Attach an Image you want to fade in the GameObject's Inspector
+    public Image m_Image;
+
+    //Use this to tell if the toggle returns true or false
+    public bool m_Fading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the toggle returns true, fade in the Image
+        if (m_Fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 1
+            m_Image.CrossFadeAlpha(1, 1.0f, false);
+            //m_Text.CrossFadeAlpha(1, 0.5f, false);
+        }
+        //If the toggle is false, fade out to nothing (0) the Image with a duration of 1
+        if (m_Fading == false)
+        {
+            m_Image.CrossFadeAlpha(0, 1.0f, false);
+            //m_Text.CrossFadeAlpha(0, 0.5f, false);
+        }
+    }
+
+    public void toggle_fade()
+    {
+        m_Fading = !m_Fading;
+    }
+
+}
--- Assets/Scripts/GameHelper.cs
+++ Assets/Scripts/GameHelper.cs
@@ -11,12 +11,20 @@ public class GameHelper : MonoBehaviour
     public Transform player;
     public Transform boulder;
     public Text distance;
+
+    //Attach an Image you want to fade in the GameObject's Inspector
+    public Image m_Image;
+    //Use this to tell if the toggle returns true or false
+    public bool m_Fading;
+
+    public Animator anim;
     private ObjectPoolNS pool;
     // Start is called before the first frame update
     void Start()
     {
         //reviveButtonRect.anchoredPosition = new Vector2(0, 500);
         pool = GameObject.Find("Tilemap").GetComponent<ObjectPoolNS>();
+        anim = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
@@ -40,6 +48,23 @@ public class GameHelper : MonoBehaviour
         {
             start_button.SetActive(false);
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //m_Fading = !m_Fading;
+        }
+
+
+        if (m_Fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 1
+            m_Image.CrossFadeAlpha(1, 1.0f, false);
+        }
+        //If the toggle is false, fade out to nothing (0) the Image with a duration of 1
+        if (m_Fading == false)
+        {
+            m_Image.CrossFadeAlpha(0, 1.0f, false);
+        }
     }
 
     public void Start_Game()
@@ -53,8 +78,21 @@ public class GameHelper : MonoBehaviour
         reviveButtonRect.anchoredPosition = Vector2.Lerp(reviveButtonRect.anchoredPosition, new Vector2(0, 0), 5f * Time.deltaTime);
     }
 
+    public IEnumerator Reload()
+    {
+        anim.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+        Restart();
+        yield return new WaitForSeconds(.5f);
+        anim.SetTrigger("End");
+    }
+    public void Reload_Restart()
+    {
+        StartCoroutine("Reload");
+    }
     public void Restart()
     {
+
         pool.HideAllGridChild();
         
         for (int i = 0; i < start_tilemaps.Length; i++)
