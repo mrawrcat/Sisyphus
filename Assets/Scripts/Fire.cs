7ce5762742ab544570024307555d6ec589b677f5
mrawrcat <wdan13@gmail.com>
2020-09-11 19:25:04 -0400

    Added Fire

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            anim.SetTrigger("hit");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            anim.SetTrigger("hit");
+        }
+    }
+}
